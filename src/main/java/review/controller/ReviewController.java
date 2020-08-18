081f87609bbbc72b3ab850567cf30dac73413f92
YangChoi <yang.hj.r@gmail.com>
2020-08-18 20:38:52 +0900

    버전1.1

--- src/main/java/review/controller/ReviewController.java
+++ src/main/java/review/controller/ReviewController.java
@@ -8,16 +8,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import review.service.ReviewService;
 
 @Controller
-@RequestMapping(value="review")
+@RequestMapping(value = "review")
 public class ReviewController {
 
 	@Autowired
 	private ReviewService reviewService;
-	
+
 	// 리뷰 리스트 (nonSearch)
-	@RequestMapping(value="reviewNonSearchList", method=RequestMethod.GET)
+	@RequestMapping(value = "reviewNonSearchList", method = RequestMethod.GET)
 	public String reviewNonSearchList() {
-		
+
 		return "/jsp/review/review_nonSearchList";
+	}
+
+	// 리뷰 리스트(search)
+	@RequestMapping(value = "review_searchList", method = RequestMethod.GET)
+	public String review_searchList() {
+		return "/jsp/review/review_searchList";
+	}
+
+	// 리뷰 작성하는 페이지(writeForm)
+	@RequestMapping(value = "review_writeForm", method = RequestMethod.GET)
+	public String review_writeForm() {
+		return "/jsp/review/review_writeForm";
+	}
+
+	// 리뷰 페이지(searchView)
+	@RequestMapping(value = "reviewView", method = RequestMethod.GET)
+	public String reviewView() {
+		return "/jsp/review/reviewView";
 	}
 }
