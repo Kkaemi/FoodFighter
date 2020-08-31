274e51805b379493b54de7604319ed7efa52b36e
yeeeerin <jyr950401@gmail.com>
2020-08-31 14:38:44 +0900

    yeeeerin

--- src/main/java/review/controller/ReviewController.java
+++ src/main/java/review/controller/ReviewController.java
@@ -3,15 +3,25 @@ package review.controller;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import manager.bean.StoreDTO;
+import member.bean.MemberDTO;
 import review.service.ReviewService;
 
 @Controller
@@ -23,22 +33,21 @@ public class ReviewController {
 	// 리뷰 리스트 (nonSearch)
 	@RequestMapping(value="reviewNonSearchList", method=RequestMethod.GET)
 	public String reviewNonSearchList() {
-		
 		return "/jsp/review/review_nonSearchList";
 	}
-	
-	//리뷰 리스트(search)
-	@RequestMapping(value="review_searchList", method=RequestMethod.GET)
-	public String review_searchList() {
-		return "/jsp/review/review_searchList";
-	}
-	
+
 	//리뷰 작성하는 페이지(writeForm)
 	@RequestMapping(value="review_writeForm", method=RequestMethod.GET)
 	public String review_writeForm() {
 		return "/jsp/review/review_writeForm";
 	}
 	
+	//리뷰 리스트(review_searchList)
+	@RequestMapping(value="review_searchList", method=RequestMethod.GET)
+	public String review_searchList() {
+		return "/jsp/review/review_searchList";
+	}
+		
 	//리뷰 페이지(searchView)
 	@RequestMapping(value="reviewView", method=RequestMethod.GET)
 	public String reviewView() {
@@ -47,8 +56,10 @@ public class ReviewController {
 	
 	//리뷰 DB저장 
 	@RequestMapping(value="writeReview", method=RequestMethod.POST)
-	public String writeReview(@RequestParam Map<String, String> map,
-							  @RequestParam("img[]") List<MultipartFile> list){
+	public String writeReview(@RequestParam Map<String, Object> map,
+							  @RequestParam("img[]") List<MultipartFile> list,
+							  HttpSession session){
+		
 		String filePath = "/Users/jiyelin/Documents/GitHub/FoodFighter/src/main/webapp/storage/review";
 		
 		int i=1;
@@ -71,16 +82,41 @@ public class ReviewController {
 		for(; i<=5; i++) {
 			map.put("image"+i, "");
 		}
-			map.put("member_seq","");
-			map.put("nickname","");
+		
+			MemberDTO memberDTO =(MemberDTO)session.getAttribute("memberDTO");
+//			StoreDTO storeDTO =(StoreDTO)session.getAttribute("storeDTO");
+			
+			map.put("nickname", memberDTO.getNickname());
+			map.put("member_seq",memberDTO.getMember_seq());
 			map.put("storename","");
+//			map.put("storename",storeDTO.getStoreName());
 		//DB
 		reviewService.writeReview(map);
 		
 		return "/jsp/review/reviewView";
 	}
 	
-	//검색어를 통한 페이지이동
+	//검색어를 통한 리스트
+	@RequestMapping(value="getSearchList", method=RequestMethod.GET)
+	@ResponseBody
+	public ModelAndView getSearchList(@RequestParam Map<String, Object> map,
+									  @RequestParam(required=false, defaultValue="1") String pg) {
+
+		//5개씩 보여지는 리스트
+		List<StoreDTO> list = reviewService.getSearchList(pg,(String)map.get("keyword"));
+
+		map.put("list",list);
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("list", list);
+		mav.addObject("pg", pg);
+		mav.addObject("keyword", map.get("keyword"));
+		mav.setViewName("/jsp/review/review_searchList");
+		
+		System.out.println(list);
+		
+		return mav;
+	}
 	
 }
 
---
+++ src/main/webapp/resources/js/review/review_writeForm.js
@@ -0,0 +1,127 @@
+$(document).ready(function(){
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
+    $(document).ajaxSend(function(e, xhr, options) {
+        xhr.setRequestHeader(header, token);
+    });
+});
+
+//유효성검사
+$(document).ready(function(){
+	$('#reviewBtn').click(function(){
+		if($('#content').val()==''){
+			alert('내용을 입력해주세요');
+		} else {
+		 /*  document.reviewForm.submit(); */
+			$.ajax({
+				type: 'post',
+				enctype:'multipart/form-data',
+				processData: false,
+				contentType: false,
+				url: 'writeReview',
+				data: new FormData($('#reviewForm')[0]),
+				success: function(){
+					alert("리뷰를 저장하였습니다");
+					location.href="reviewView";
+				},
+				error:function(err){
+					console.log(err);
+				}
+				
+			});
+		} 
+		/* return false; */
+	});
+});
+
+//다시작성 
+$('#resetBtn').click(function(){
+	if(confirm("지금까지 작성하신걸 지우시겠습니까?")){
+		$('#content').empty();
+		$('span.on').removeClass('on');
+		$('.filebox div').not('div.file_input').remove();
+		$('.ReviewPictureCounter__CurrentLength').text('0');
+		document.getElementById("ex_file").disabled=false; 
+	}else{
+		return false;
+	}
+});
+
+//별 평점
+$('.starRev>span').click(function(){
+   $(this).parent().children('span.on').removeClass('on');
+   $(this).addClass('on').prevAll('span').addClass('on');
+   let starAvg = $(this).attr('data-value');
+	$('#starAvg').val(starAvg);
+   
+   return false;
+});
+
+//이지미업로드
+let arr_total = 0;
+$(document).ready(function (e){
+  $("input[type='file']").change(function(e){
+
+    //div 내용 비워주기
+    $('#imageView').empty();
+
+    let files = e.target.files;
+    let arr =Array.prototype.slice.call(files);
+	
+    //업로드 가능 파일인지 체크
+    for(let i=0;i<files.length;i++){
+      if(!checkExtension(files[i].name,files[i].size)){
+        return false;
+      }
+    }
+    
+    preview(arr);
+    
+    
+  });//file change
+  
+  function checkExtension(fileName,fileSize){
+
+  	let regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
+  	let maxSize = 20971520;  //20MB
+    
+    if(fileSize >= maxSize){
+      alert('파일 사이즈 초과');
+      $("input[type='file']").val("");  //파일 초기화
+      return false;
+    }
+    
+    if(regex.test(fileName)){
+      alert('업로드 불가능한 파일이 있습니다.');
+      $("input[type='file']").val("");  //파일 초기화
+      return false;
+    }
+    return true;
+  }
+  
+  
+  function preview(arr){
+      arr_total += arr.length; 
+  	if(arr_total < 6){
+	    	arr.forEach(function(f){
+	        //div에 이미지 추가
+	        let str = '<div style="display:inline-block; list-style:none; line-height: 89px; text-align: center;">';
+	        //이미지 파일 미리보기
+	        if(f.type.match('image.*')){
+      	 let reader = new FileReader(); //파일을 읽기 위한 FileReader객체 생성
+	          	reader.onload = function (e) { //파일 읽어들이기를 성공했을때 호출되는 이벤트 핸들러
+	       		  str += '<img src="'+e.target.result+'"id=viewImg style="display:inline-block; list-style:none;"'+'"width=89px; height=89px; />';
+	   	          str += '</div>';
+	   	           $(str).prependTo('.filebox');
+	  	          }
+		          reader.readAsDataURL(f);
+		        }
+	      });//arr.forEach
+      }//if
+      else{
+          alert('최대 5개만 업로드 가능합니다');
+          arr_total -= arr.length; 
+      }
+      $('.ReviewPictureCounter__CurrentLength').text(arr_total); //사진개수 입력란
+	}//preview
+});
